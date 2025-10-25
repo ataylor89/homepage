631d65fbd361096286645fdd401b487b63696dbe
Andrew Taylor <rolyata47@gmail.com>
2025-10-24 22:48:22 -0500

    Adding banner, navbar, and weather.html

---
+++ homepage/views.py
@@ -0,0 +1,26 @@
+from flask import request, render_template, jsonify
+from homepage import app, weather
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['GET'])
+def weather_forecast():
+    return render_template('weather.html')
+
+@app.route('/forecast', methods=['POST'])
+def forecast():
+    param = request.form['search_param']
+    if param == 'city':
+        city = request.form['city']
+        latitude, longitude = weather.geocode(city)
+    else:
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        city = weather.rev_geocode(latitude, longitude)
+    forecast = weather.get_forecast(latitude, longitude)
+    forecast['city'] = city
+    forecast['latitude'] = latitude
+    forecast['longitude'] = longitude
+    return jsonify(forecast)
---
+++ homepage/weather.py
@@ -0,0 +1,47 @@
+import requests
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent='weather')
+
+def get_forecast(latitude, longitude):
+    url1 = f'https://api.weather.gov/points/{latitude},{longitude}'
+    resp1 = requests.get(url1)
+    url2 = resp1.json()['properties']['forecast']
+    resp2 = requests.get(url2)
+    data = resp2.json()['properties']
+    forecast = format_json(data)
+    return forecast
+
+def format_json(data):
+    forecast = {}
+    forecast['elevation'] = f'{0} m'.format(data['elevation']['value'])
+    forecast['generatedAt'] = data['generatedAt']
+    forecast['units'] = data['units']
+    forecast['periods'] = []
+    for src in data['periods']:
+        dst = {}
+        dst['name'] = src['name']
+        dst['temperature'] = str(src['temperature']) + ' ' + src['temperatureUnit']
+        if src['probabilityOfPrecipitation']['value'] is None:
+            dst['probabilityOfPrecipitation'] = 'N/A'
+        else:
+            dst['probabilityOfPrecipitation'] = str(src['probabilityOfPrecipitation']['value']) + '%'
+        dst['windDirection'] = src['windDirection']
+        dst['windSpeed'] = src['windSpeed']
+        dst['shortForecast'] = src['shortForecast']
+        dst['detailedForecast'] = src['detailedForecast']
+        dst['icon'] = src['icon']
+        forecast['periods'].append(dst)
+    return forecast
+
+def geocode(city):
+    location = geolocator.geocode(city)
+    return (location.latitude, location.longitude)
+
+def rev_geocode(latitude, longitude):
+    location = geolocator.reverse(format('%s, %s' %(latitude, longitude)), language='en')
+    if location.raw['addresstype'] == 'city':
+        return location.raw['name']
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 1.8 + 32
