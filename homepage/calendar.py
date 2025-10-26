81c2bdd5b4598144943d4918fa91dd758705d282
Andrew Taylor <rolyata47@gmail.com>
2025-10-26 05:38:18 -0500

    Adding calendar feature

---
+++ homepage/calendar.py
@@ -0,0 +1,137 @@
+from datetime import datetime
+
+def current():
+    now = datetime.now()
+    return get(now.year)
+
+def get(year):
+    if year in calendars:
+        return {
+            'metadata': calendars['metadata'],
+            'calendar': calendars[year]
+        }
+
+calendars = {
+    'metadata': {
+        'min_year': 2025,
+        'max_year': 2025
+    },
+    2025: {
+        'year': 2025,
+        'leap_year': False,
+        'months': [
+            {
+                'month': 1,
+                'monthname': 'January',
+                'days': 31,
+                'offset': 3,
+                'entries': {
+                    1: 'New Year\'s Day'
+                }
+            },
+            {
+                'month': 2,
+                'monthname': 'February',
+                'days': 28,
+                'offset': 6,
+                'entries': {
+                    14: 'Valentine\'s Day'
+                }
+            },
+            {
+                'month': 3,
+                'monthname': 'March',
+                'days': 31,
+                'offset': 6,
+                'entries': {
+                    17: 'Saint Patrick\'s Day',
+                    20: 'March Equinox'
+                }
+            },
+            {
+                'month': 4,
+                'monthname': 'April',
+                'days': 30,
+                'offset': 2,
+                'entries': {
+                    20: 'Easter Sunday',
+                    21: 'Easter Monday'
+                }
+            },
+            {
+                'month': 5,
+                'monthname': 'May',
+                'days': 31,
+                'offset': 4,
+                'entries': {
+                    11: 'Mother\'s Day'
+                }
+            },
+            {
+                'month': 6,
+                'monthname': 'June',
+                'days': 30,
+                'offset': 0,
+                'entries': {
+                    15: 'Father\'s Day',
+                    19: 'Juneteenth',
+                    21: 'June Solstice'
+                }
+            },
+            {
+                'month': 7,
+                'monthname': 'July',
+                'days': 31,
+                'offset': 2,
+                'entries': {}
+            },
+            {
+                'month': 8,
+                'monthname': 'August',
+                'days': 31,
+                'offset': 5,
+                'entries': {}
+            },
+            {
+                'month': 9,
+                'monthname': 'September',
+                'days': 30,
+                'offset': 1,
+                'entries': {
+                    1: 'Labor Day',
+                    22: 'September Equinox'
+                }
+            },
+            {
+                'month': 10,
+                'monthname': 'October',
+                'days': 31,
+                'offset': 3,
+                'entries': {
+                    31: 'Halloween'
+                }
+            },
+            {
+                'month': 11,
+                'monthname': 'November',
+                'days': 30,
+                'offset': 6,
+                'entries': {
+                    27: 'Thanksgiving Day'
+                }
+            },
+            {
+                'month': 12,
+                'monthname': 'December',
+                'days': 31,
+                'offset': 1,
+                'entries': {
+                    21: 'December Solstice',
+                    24: 'Christmas Eve',
+                    25: 'Christmas Day',
+                    31: 'New Year\'s Eve'
+                }
+            }
+        ]
+    }
+}
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from flask import request, render_template, jsonify
-from homepage import app, weather
+from homepage import app, weather, calendar
 
 @app.route('/', methods=['GET'])
 def home():
@@ -24,3 +24,20 @@ def forecast():
     forecast['latitude'] = latitude
     forecast['longitude'] = longitude
     return jsonify(forecast)
+
+@app.route('/calendar', methods=['GET'])
+def calendar_view():
+    return render_template('calendar.html')
+
+@app.route('/calendar_data', methods=['POST'])
+def calendar_data():
+    year = int(request.form['year'])
+    return jsonify(calendar.get(year))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
