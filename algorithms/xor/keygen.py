b0dfbc493432806eebbea02151bae136b891c604
Andrew Taylor <rolyata47@gmail.com>
2025-10-29 11:13:56 -0500

    Small improvements

--- algorithms/xor/keygen.py
+++ algorithms/xor/keygen.py
@@ -5,7 +5,7 @@ def gen_key(keylen):
     key = [chr(random.randint(0, 255)) for i in range(0, keylen)]
     return "".join(key)
 
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="keygen.py", description="Create an XOR key", epilog="Thanks for reading")
     parser.add_argument("-kl", "--keylength", type=int, default=1024)
     parser.add_argument("-o", "--output", type=str, default="key.txt")
@@ -15,6 +15,3 @@ def main():
     key = gen_key(keylen)
     with open(filepath, "w") as file:
         file.write(key)
-
-if __name__ == "__main__":
-    main()
