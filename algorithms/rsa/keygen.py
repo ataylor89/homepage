c55e933b27766d08dd0fd5816f95c2a64d7ea1a8
Andrew Taylor <rolyata47@gmail.com>
2025-11-02 11:59:59 -0600

    Simplifying code

--- algorithms/rsa/keygen.py
+++ algorithms/rsa/keygen.py
@@ -2,10 +2,7 @@ from algorithms.rsa import keytable
 import random
 
 def create_key_pair(keylen, tmin, tmax):
-    filtered = {}
-    for k,v in keytable.table.items():
-        if v[1] >= tmin and v[1] <= tmax and v[2] >= tmin and v[2] <= tmax:
-            filtered[k] = v
+    filtered = keytable.filter(tmin, tmax)
     if keylen > len(filtered):
         raise ValueError("There aren't enough keys in the key table that meet the criteria")
     nvalues = list(filtered.keys())
