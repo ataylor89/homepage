cb34230d155f0cd09c2db45fd335cab67de15086
Andrew Taylor <rolyata47@gmail.com>
2025-11-02 12:27:31 -0600

    Simplifying code

--- algorithms/rot13.py
+++ algorithms/rot13.py
@@ -10,23 +10,3 @@ def rot13(message):
         substitution = table[letter] if letter in table else letter
         result += substitution
     return result
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(prog="rot13.py", description="Encrypt or decrypt a message using rot13")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("message", type=str, nargs="?")
-    group.add_argument("-m", "--msgfile", type=str)
-    parser.add_argument("-o", "--output", type=str)
-    args = parser.parse_args()
-    if args.msgfile:
-        with open(args.msgfile, "r") as msgfile:
-            message = msgfile.read()
-    else:
-        message = args.message
-    output = rot13(message)
-    if args.output:
-        with open(args.output, "w") as outputfile:
-            outputfile.write(output)
-    else:
-        print(output, end="")
--- algorithms/rot88.py
+++ algorithms/rot88.py
@@ -5,23 +5,3 @@ def rot88(message):
         code = (code + 0x88000) % 0x110000
         result += chr(code)
     return result
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(prog="rot88.py", description="Encrypt or decrypt a message using rot88")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("message", type=str, nargs="?")
-    group.add_argument("-m", "--msgfile", type=str)
-    parser.add_argument("-o", "--output", type=str)
-    args = parser.parse_args()
-    if args.msgfile:
-        with open(args.msgfile, "r") as msgfile:
-            message = msgfile.read()
-    else:
-        message = args.message
-    output = rot88(message)
-    if args.output:
-        with open(args.output, "w") as outputfile:
-            outputfile.write(output)
-    else:
-        print(output, end="")
