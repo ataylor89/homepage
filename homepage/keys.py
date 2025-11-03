510751ca3715ed1a77eb200cd2c4c3cef5b16a5b
Andrew Taylor <rolyata47@gmail.com>
2025-11-03 08:13:54 -0600

    Using a default key for now

--- homepage/keys.py
+++ homepage/keys.py
@@ -9,115 +9,47 @@ import sys
 
 keys = {'RSA': {}, 'XOR': {}}
 
-def init():
-    project_root = sys.path[0]
-    path = f'{project_root}/algorithms/rsa/keys'
-    try:
-        os.makedirs(path, exist_ok=False)
-        print('Created directory ' + path)
-    except: pass
-    path = f'{project_root}/algorithms/xor/keys'
-    try:
-        os.makedirs(path, exist_ok=False)
-        print('Created directory ' + path)
-    except: pass
-
-def load_from_files():
-    project_root = sys.path[0]
-    path = f'{project_root}/algorithms/rsa/keys/'
-
-    counts = [0, 0]
-
-    if os.path.isfile(path + 'small.txt'):
-        try:
-            keys['RSA']['small'] = rsa_keyio.load(path + 'small.txt')
-            counts[0] += 1
-            print('Loaded key from file %s' %(path + 'small.txt'))
-        except Exception as err:
-            print(err)
-
-    if os.path.isfile(path + 'medium.txt'):
-        try:
-            keys['RSA']['medium'] = rsa_keyio.load(path + 'medium.txt')
-            counts[0] += 1
-            print('Loaded key from file %s' %(path + 'medium.txt'))
-        except Exception as err:
-            print(err)
-
-    if os.path.isfile(path + 'large.txt'):
-        try:
-            keys['RSA']['large'] = rsa_keyio.load(path + 'large.txt')
-            counts[0] += 1
-            print('Loaded key from file %s' %(path + 'large.txt'))
-        except Exception as err:
-            print(err)
-
-    path = f'{project_root}/algorithms/xor/keys/'
-
-    if os.path.isfile(path + 'small.txt'):
-        try:
-            keys['XOR']['small'] = xor_keyio.load(path + 'small.txt')
-            counts[1] += 1
-            print('Loaded key from file %s' %(path + 'small.txt'))
-        except Exception as err:
-            print(err)
-
-    if os.path.isfile(path + 'medium.txt'):
-        try:
-            keys['XOR']['medium'] = xor_keyio.load(path + 'medium.txt')
-            counts[1] += 1
-            print('Loaded key from file %s' %(path + 'medium.txt'))
-        except Exception as err:
-            print(err)
-
-    if os.path.isfile(path + 'large.txt'):
-        try:
-            keys['XOR']['large'] = xor_keyio.load(path + 'large.txt')
-            counts[1] += 1
-            print('Loaded key from file %s' %(path + 'large.txt'))
-        except Exception as err:
-            print(err)
-
-    return counts
-
 def load():
-    init()
-    counts = load_from_files()
     project_root = sys.path[0]
 
-    if counts[0] < 3:
-        path = f'{project_root}/algorithms/rsa/keys/'
+    path = f'{project_root}/algorithms/rsa/keys/'
+    try:
+        os.makedirs(path, exist_ok=False)
+        print('Created directory ' + path)
+    except: pass
+
+    if os.path.isfile(path + 'default.txt'):
+        try:
+            keys['RSA']['default'] = rsa_keyio.load(path + 'default.txt')
+            print(f'Loaded key from file {path}default.txt')
+        except Exception as err:
+            print(err)
+
+    if 'default' not in keys['RSA'] or keys['RSA']['default'] is None:
         rsa_primetable.load()
         rsa_keytable.load()
         rsa_primetable.generate(1000)
         rsa_keytable.generate(64, 10, 1000)
-        rsa_keytable.generate(64, 100, 1000)
-        rsa_keytable.generate(64, 500, 1000)
         rsa_primetable.save()
         rsa_keytable.save()
-        if 'small' not in keys['RSA']:
-            keys['RSA']['small'] = rsa_keygen.create_key_pair(64, 10, 1000)
-            rsa_keyio.save(keys['RSA']['small'], path + 'small.txt')
-            print(f'Saved key to file {path}small.txt')
-        if 'medium' not in keys['RSA']:
-            keys['RSA']['medium'] = rsa_keygen.create_key_pair(64, 100, 1000)
-            rsa_keyio.save(keys['RSA']['medium'], path + 'medium.txt')
-            print(f'Saved key to file {path}medium.txt')
-        if 'large' not in keys['RSA']:
-            keys['RSA']['large'] = rsa_keygen.create_key_pair(64, 500, 1000)
-            rsa_keyio.save(keys['RSA']['large'], path + 'large.txt')
-            print(f'Saved key to file {path}large.txt')
-    if counts[1] < 3:
-        path = f'{project_root}/algorithms/xor/keys/'
-        if 'small' not in keys['XOR']:
-            keys['XOR']['small'] = xor_keygen.create_key(64)
-            xor_keyio.save(keys['XOR']['small'], path + 'small.txt')
-            print(f'Saved key to file {path}small.txt')
-        if 'medium' not in keys['XOR']:
-            keys['XOR']['medium'] = xor_keygen.create_key(256)
-            xor_keyio.save(keys['XOR']['medium'], path + 'medium.txt')
-            print(f'Saved key to file {path}medium.txt')
-        if 'large' not in keys['XOR']:
-            keys['XOR']['large'] = xor_keygen.create_key(1024)
-            xor_keyio.save(keys['XOR']['large'], path + 'large.txt')
-            print(f'Saved key to file {path}large.txt')
+        keys['RSA']['default'] = rsa_keygen.create_key_pair(64, 10, 1000)
+        rsa_keyio.save(keys['RSA']['default'], path + 'default.txt')
+        print(f'Saved key to file {path}default.txt')
+
+    path = f'{project_root}/algorithms/xor/keys/'
+    try:
+        os.makedirs(path, exist_ok=False)
+        print('Created directory ' + path)
+    except: pass
+
+    if os.path.isfile(path + 'default.txt'):
+        try:
+            keys['XOR']['default'] = xor_keyio.load(path + 'default.txt')
+            print(f'Loaded key from file {path}default.txt')
+        except Exception as err:
+            print(err)
+
+    if 'default' not in keys['XOR'] or keys['XOR']['default'] is None:
+        keys['XOR']['default'] = xor_keygen.create_key(1024)
+        xor_keyio.save(keys['XOR']['default'], path + 'default.txt')
+        print(f'Saved key to file {path}default.txt')
