1cee2963f3a3cedf3fcec77b50efe9df4e42e655
Andrew Taylor <rolyata47@gmail.com>
2025-11-03 11:56:31 -0600

    Making some small improvements

--- homepage/keys.py
+++ homepage/keys.py
@@ -1,31 +1,31 @@
+import os
+import sys
 from algorithms.rsa import primetable as rsa_primetable
 from algorithms.rsa import keytable as rsa_keytable
 from algorithms.rsa import keygen as rsa_keygen
 from algorithms.rsa import keyio as rsa_keyio
 from algorithms.xor import keygen as xor_keygen
 from algorithms.xor import keyio as xor_keyio
-import os
-import sys
 
 keys = {'RSA': {}, 'XOR': {}}
 
 def load():
     project_root = sys.path[0]
-    rsa_path = f'{project_root}/algorithms/rsa/keys/'
-    xor_path = f'{project_root}/algorithms/xor/keys/'
+    rsa_key_path = f'{project_root}/algorithms/rsa/keys/'
+    xor_key_path = f'{project_root}/algorithms/xor/keys/'
 
     try:
-        os.makedirs(rsa_path, exist_ok=False)
-        print('Created directory ' + rsa_path)
+        os.makedirs(rsa_key_path, exist_ok=False)
+        print('Created directory ' + rsa_key_path)
     except: pass
 
     try:
-        os.makedirs(xor_path, exist_ok=False)
-        print('Created directory ' + xor_path)
+        os.makedirs(xor_key_path, exist_ok=False)
+        print('Created directory ' + xor_key_path)
     except: pass
 
-    for item in os.listdir(rsa_path):
-        full_path = os.path.join(rsa_path, item)
+    for item in os.listdir(rsa_key_path):
+        full_path = os.path.join(rsa_key_path, item)
         if os.path.isfile(full_path) and item.endswith('.key'):
             keyname = item[:-4]
             try:
@@ -42,11 +42,11 @@ def load():
         rsa_primetable.save()
         rsa_keytable.save()
         keys['RSA']['default'] = rsa_keygen.create_key_pair(64, 10, 1000)
-        rsa_keyio.save(keys['RSA']['default'], rsa_path + 'default.key')
-        print(f'Saved key to file {rsa_path}default.key')
+        rsa_keyio.save(keys['RSA']['default'], rsa_key_path + 'default.key')
+        print(f'Saved key to file {rsa_key_path}default.key')
 
-    for item in os.listdir(xor_path):
-        full_path = os.path.join(xor_path, item)
+    for item in os.listdir(xor_key_path):
+        full_path = os.path.join(xor_key_path, item)
         if os.path.isfile(full_path) and item.endswith('.key'):
             keyname = item[:-4]
             try:
@@ -57,11 +57,10 @@ def load():
 
     if 'default' not in keys['XOR'] or keys['XOR']['default'] is None:
         keys['XOR']['default'] = xor_keygen.create_key(1024)
-        xor_keyio.save(keys['XOR']['default'], xor_path + 'default.key')
-        print(f'Saved key to file {xor_path}default.key')
+        xor_keyio.save(keys['XOR']['default'], xor_key_path + 'default.key')
+        print(f'Saved key to file {xor_key_path}default.key')
 
 def get_key_names():
-    return {
-        'RSA': sorted(list(keys['RSA'].keys())),
-        'XOR': sorted(list(keys['XOR'].keys()))
-    }
+    rsa_keys = sorted(list(keys['RSA'].keys()))
+    xor_keys = sorted(list(keys['XOR'].keys()))
+    return {'RSA': rsa_keys, 'XOR': xor_keys}
