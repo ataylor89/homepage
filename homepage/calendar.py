14f9ebac9ac754b881a131ad7d405c511e4aedc2
Andrew Taylor <rolyata47@gmail.com>
2025-11-03 18:47:56 -0600

    Using absolute paths (instead of relative paths) so that I can use a symbolic link

--- homepage/calendar.py
+++ homepage/calendar.py
@@ -1,9 +1,11 @@
 import json
+import sys
 import os
 
 def get(year):
-    metadata_path = f'homepage/static/data/calendar/metadata.json'
-    calendar_path = f'homepage/static/data/calendar/{year}.json'
+    project_root = sys.path[0]
+    metadata_path = f'{project_root}/homepage/static/data/calendar/metadata.json'
+    calendar_path = f'{project_root}/homepage/static/data/calendar/{year}.json'
     if os.path.isfile(metadata_path) and os.path.isfile(calendar_path):
         metadata_file = open(metadata_path, 'r')
         calendar_file = open(calendar_path, 'r')
--- homepage/dictionary.py
+++ homepage/dictionary.py
@@ -1,8 +1,10 @@
 import json
+import sys
 import os
 
 def create_dictionary(subjects):
-    path = 'homepage/static/data/dictionary/dictionary.json'
+    project_root = sys.path[0]
+    path = f'{project_root}/homepage/static/data/dictionary/dictionary.json'
     if os.path.isfile(path):
         with open(path, 'r') as file:
             data = json.load(file)
