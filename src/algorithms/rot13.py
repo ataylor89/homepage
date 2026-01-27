47bc9a91bdc820780f7ffb8010cd3689bda3d186
Andrew Taylor <rolyata47@gmail.com>
2026-01-27 17:00:43 -0600

    Simplifying the cryptography code

--- src/algorithms/rot13.py
+++ src/algorithms/rot13.py
@@ -1,11 +1,11 @@
 table = {}
 
 for i in range(0, 26):
-    table[chr(ord("a") + i)] = chr(ord("a") + (i + 13) % 26)
-    table[chr(ord("A") + i)] = chr(ord("A") + (i + 13) % 26)
+    table[chr(ord('a') + i)] = chr(ord('a') + (i + 13) % 26)
+    table[chr(ord('A') + i)] = chr(ord('A') + (i + 13) % 26)
 
 def rot13(message):
-    result = ""
+    result = ''
     for letter in message:
         substitution = table[letter] if letter in table else letter
         result += substitution
